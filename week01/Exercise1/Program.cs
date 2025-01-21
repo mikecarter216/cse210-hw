213fdd4aafcb02592be048300f872d3b2f2fd8fd
mikecarter216 <michaelakpan216@gmail.com>
2025-01-21 17:58:47 +0100

    excercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Prompt for input and store it in a variable
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        
+        // Output the user's input with string interpolation
+        Console.WriteLine($"Your favorite color is {color}.");
     }
 }
